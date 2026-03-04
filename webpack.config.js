379ce7029454ad82bf9666fe61baa1aee6f40e5b
acekamaa <kvnkamaa@gmail.com>
2026-03-04 06:05:29 +0300

    docs: Refactor and rearranging of project architecture

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/scripts/index.js',
+
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+
+    mode: isProduction ? "production" : "development",
+
+    devtool: isProduction ? false : "source-map",
+
+    devServer: {
+        static: "./dist",
+        port: 8080,
+        open: true,
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+
+    resolve: {
+        extensions: [".js"],
+    },
+  };
+};
